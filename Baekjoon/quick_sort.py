631a5c5545a9d524724ee72b4a246580850eb946
Jeonhyunil <junhyeonill@gmail.com>
2018-06-09 15:30:32 +0900

    add explanation

--- Baekjoon/quick_sort.py
+++ Baekjoon/quick_sort.py
@@ -16,6 +16,16 @@ quick sort를 구현하는 프로그램을 작성하세요.
 
 #My Code
 def left_side(s):
+    if len(s) <= 1: # 기저조건 = 문장의 길이가 1보다 작거나 같으면 그냥 출력
+        return s
+    pivot = s[len(s)//2] # 문장의 중간쯤의 숫자를 pivot으로 설정
+    middle = [x for x in s if x == pivot ] # pivot이 여러개일 수 있으므로 골라내준다.
+    left = [x for x in s if x < pivot] # pivot보다 작은 숫자들을 left에 저장
+    right = [x for x in s if x > pivot] # pivot보다 큰 숫자들을 right에 저장
+
+    return left_side(left) + middle + right_side(right) # left와 right 들은 각각에 위치에 맞는 함수(left_side(), right_side())로 재귀시켜준다.
+
+def right_side(s): # 위와 마찬가지
     if len(s) <= 1:
         return s
     pivot = s[len(s)//2]
@@ -25,17 +35,7 @@ def left_side(s):
 
     return left_side(left) + middle + right_side(right)
 
-def right_side(s):
-    if len(s) <= 1:
-        return s
-    pivot = s[len(s)//2]
-    middle = [x for x in s if x == pivot ]
-    left = [x for x in s if x < pivot]
-    right = [x for x in s if x > pivot]
-
-    return left_side(left) + middle + right_side(right)
-
-def quick_sort(s):
+def quick_sort(s): # 위와 마찬가지
     pivot = s[len(s)//2]
     middle = [x for x in s if x == pivot ]
     left = [x for x in s if x < pivot]
